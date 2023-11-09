b44da423d1baabc8b2ab91679eea8352139fc264
dwprz <dwiprasetiyo223@gmail.com>
2023-11-09 13:39:10 +0700

    corrections

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -4,17 +4,23 @@ def call () {
         stages{
             stage("Maven Clean") {
                 steps{
+                  script{
                     maven(["clean"])
+                  }
                 }
             }
             stage("Maven Compile") {
                 steps{
+                  script{
                     maven(["compile"])
+                  }
                 }
             }
             stage("Maven Test") {
                 steps{
+                  script{
                     maven(["test"])
+                  }
                 }
             }
         }       
