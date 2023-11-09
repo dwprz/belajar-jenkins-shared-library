7d1d008fea23a9f6c0617fbe2ca3bf55eb8ad302
dwprz <dwiprasetiyo223@gmail.com>
2023-11-09 13:53:28 +0700

    add tksmPipeline.groovy

---
+++ vars/tksmPipeline.groovy
@@ -0,0 +1,18 @@
+def call (Map config) {
+  if (config.type == "Maven") {
+    mavenPipeline()
+  } else {
+    pipeline{
+        agent any
+        stages{
+            stage("Unsuported Pipeline"){
+                steps{
+                  script{
+                    echo("Unsuported Pipeline")
+                  }
+                }
+            }
+        }
+    }
+  }
+}
